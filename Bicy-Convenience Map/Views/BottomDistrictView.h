c392a293d93875e9430fb45e3509abbcedeb1a8a
Chen Defore <Defore@ChendeMacBook-Pro.local>
2016-12-20 15:46:12 +0800

    添加底部栏目类。XIB自定义的UIView类 图片因为尚未最终确定，并未上传。

---
+++ Bicy-Convenience Map/Views/BottomDistrictView.h
@@ -0,0 +1,13 @@
+//
+//  BottomDistrictView.h
+//  Bicy-Convenience Map
+//
+//  Created by Chen Defore on 2016/12/20.
+//  Copyright © 2016年 Chen Defore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BottomDistrictView : UIView
+
+@end
---
+++ Bicy-Convenience Map/Views/BottomDistrictView.m
@@ -0,0 +1,21 @@
+//
+//  BottomDistrictView.m
+//  Bicy-Convenience Map
+//
+//  Created by Chen Defore on 2016/12/20.
+//  Copyright © 2016年 Chen Defore. All rights reserved.
+//
+
+#import "BottomDistrictView.h"
+
+@implementation BottomDistrictView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
